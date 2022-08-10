6d88a9d32df15d381a47a393907b9757ec912d4f
Lucas Bauer <lucassoccer95@gmail.com>
2022-08-10 06:38:10 -0500

    Look at the returns
    The even_odd method is looking at the difference between implicit and
    explicit returns. Use explicit when you want to method to end at a
    certain point. Otherwise ruby will return the last line of code.

--- hello.rb
+++ hello.rb
@@ -10,6 +10,22 @@ def greet(name = "Stranger Danger")
     "Hello, " + name + "!"
 end
 
-puts greet("Bobby B.")
-puts greet("Susie")
-puts greet
+# puts greet("Bobby B.")
+# puts greet("Susie")
+# puts greet
+
+def even_odd(number)
+    unless number.is_a? Numeric
+      return "A number was not entered."
+    end
+  
+    if number % 2 == 0
+      "That is an even number."
+    else
+      "That is an odd number."
+    end
+  end
+
+  puts even_odd(20)
+  puts even_odd(21)
+  puts even_odd("Jimmy") #Needs the return in method to catch error
